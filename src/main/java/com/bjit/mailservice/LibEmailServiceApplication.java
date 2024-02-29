bd54e2cced871749d6e66cdc390bd366dbfa59af
Khlaid Hasan-BJIT||R&D <khalid.hasan.bjit@gmail.com>
2024-02-29 12:40:38 +0600

    adding main project to this repo

---
+++ src/main/java/com/bjit/mailservice/LibEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bjit.mailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibEmailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibEmailServiceApplication.class, args);
+    }
+
+}
